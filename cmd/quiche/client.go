4bc42f5406d1e10f5eb0baba6338cb46e7361ce0
Quoc-Viet Nguyen <afelion@gmail.com>
2019-08-14 23:26:21 +1000

    Server example

---
+++ cmd/quiche/client.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/goburrow/quiche"
+)
+
+func hostname(addr string) string {
+	idx := strings.LastIndex(addr, ":")
+	if idx < 0 {
+		return addr
+	}
+	return addr[:idx]
+}
+
+func dialUDP(addr string) (net.Conn, error) {
+	localAddr, err := net.ResolveUDPAddr("udp", "0.0.0.0:0")
+	if err != nil {
+		return nil, err
+	}
+	remoteAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return net.DialUDP("udp", localAddr, remoteAddr)
+}
+
+func connect(config *quiche.Config, addr string) error {
+	socket, err := dialUDP(addr)
+	if err != nil {
+		return err
+	}
+	defer socket.Close()
+
+	scid := newConnID()
+	conn := quiche.Connect(hostname(addr), scid, config)
+	defer conn.Free()
+
+	c := client{
+		socket: socket,
+		conn:   conn,
+	}
+	return c.connect()
+}
+
+type client struct {
+	socket net.Conn
+	conn   *quiche.Connection
+}
+
+func (c *client) connect() error {
+	buf := make([]byte, bufferSize)
+	err := c.send(buf)
+	if err != nil {
+		return err
+	}
+	log.Print("sent initial packet")
+	reqSent := false
+	for {
+		err = c.recv(buf)
+		if err != nil {
+			return err
+		}
+		if c.conn.IsClosed() {
+			log.Print("connection closed")
+			return nil
+		}
+		if c.conn.IsEstablished() {
+			if !reqSent {
+				log.Print("sending HTTP request")
+				_, err = c.conn.StreamSend(httpRequestStreamID, []byte("GET /\r\n"), true)
+				if err != nil {
+					return err
+				}
+				reqSent = true
+			}
+			c.recvStream(buf)
+		}
+		err = c.send(buf)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (c *client) readDeadline() time.Time {
+	timeout := c.conn.Timeout()
+	if timeout > 0 {
+		return time.Now().Add(timeout)
+	}
+	return time.Time{}
+}
+
+func (c *client) recv(buf []byte) error {
+	deadline := c.readDeadline()
+	err := c.socket.SetReadDeadline(deadline)
+	if err != nil {
+		return err
+	}
+	n, err := c.socket.Read(buf)
+	if err != nil {
+		if err, ok := err.(net.Error); ok && err.Timeout() {
+			log.Print("timed out")
+			c.conn.OnTimeout()
+			return nil
+		}
+		return err
+	}
+	_, err = c.conn.Recv(buf[:n])
+	if err == quiche.ErrDone {
+		return nil
+	}
+	return err
+}
+
+func (c *client) recvStream(buf []byte) {
+	for {
+		id, ok := c.conn.ReadableNext()
+		if !ok {
+			return
+		}
+		n, fin, err := c.conn.StreamRecv(id, buf)
+		if err != nil {
+			log.Printf("stream %d recv failed: %v", id, err)
+			continue
+		}
+		log.Printf("stream %d has %d bytes (fin=%v)\n%s", id, n, fin, buf[:n])
+		if id == httpRequestStreamID {
+			log.Print("response received, closing...")
+			c.conn.Close(true, 0x00, []byte("bye"))
+		}
+	}
+}
+
+func (c *client) send(buf []byte) error {
+	buf = buf[:maxDatagramSize]
+	for {
+		n, err := c.conn.Send(buf)
+		if err == quiche.ErrDone {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		n, err = c.socket.Write(buf[:n])
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func clientCommand(args []string) error {
+	cmd := flag.NewFlagSet("client", flag.ExitOnError)
+	verbose := cmd.Bool("v", false, "enable debug logging")
+	wireVersion := cmd.Uint("wire-version", quiche.ProtocolVersion, "the version number to send to the server")
+	noVerify := cmd.Bool("no-verify", false, "don't verify server's certificate")
+	serverAddr := cmd.String("url", "localhost:4433", "QUIC server address")
+	cmd.Parse(args)
+
+	if *verbose {
+		quiche.EnableDebugLogging()
+	}
+	config, err := newConfig(uint32(*wireVersion))
+	if err != nil {
+		return err
+	}
+	if *noVerify {
+		config.VerifyPeer(false)
+	}
+	defer config.Free()
+
+	return connect(config, *serverAddr)
+}
---
+++ cmd/quiche/server.go
@@ -0,0 +1,289 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"time"
+
+	"github.com/goburrow/quiche"
+)
+
+const maxTokenLen = 64
+
+func listenUDP(addr string) (net.PacketConn, error) {
+	localAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return net.ListenUDP("udp", localAddr)
+}
+
+func listen(config *quiche.Config, listenAddr, root string) error {
+	socket, err := listenUDP(listenAddr)
+	if err != nil {
+		return err
+	}
+	defer socket.Close()
+	s := server{
+		config: config,
+		socket: socket,
+		conns:  make(map[string]serverConn),
+	}
+	log.Printf("listening: %v", socket.LocalAddr())
+	return s.listen()
+}
+
+type serverConn struct {
+	addr net.Addr
+	conn *quiche.Connection
+}
+
+type server struct {
+	config *quiche.Config
+	socket net.PacketConn
+	conns  map[string]serverConn
+
+	noRetry bool
+}
+
+func (s *server) listen() error {
+	buf := make([]byte, bufferSize)
+	header := quiche.Header{
+		SCID:  make([]byte, quiche.MaxConnIDLen),
+		DCID:  make([]byte, quiche.MaxConnIDLen),
+		Token: make([]byte, maxTokenLen),
+	}
+	for {
+		deadline := s.readDeadline()
+		err := s.socket.SetReadDeadline(deadline)
+		if err != nil {
+			return err
+		}
+		n, addr, err := s.socket.ReadFrom(buf)
+		if err != nil {
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				for _, c := range s.conns {
+					c.conn.OnTimeout()
+				}
+			} else {
+				return err
+			}
+		} else {
+			log.Printf("got %d bytes", n)
+			s.recv(buf[:n], addr, &header)
+		}
+		s.send(buf)
+		s.close()
+	}
+}
+
+func (s *server) readDeadline() time.Time {
+	var minTimeout time.Duration
+	for _, c := range s.conns {
+		timeout := c.conn.Timeout()
+		if timeout < minTimeout {
+			minTimeout = timeout
+		}
+	}
+	if minTimeout > 0 {
+		return time.Now().Add(minTimeout)
+	}
+	return time.Time{}
+}
+
+func (s *server) recv(buf []byte, addr net.Addr, h *quiche.Header) {
+	err := s.headerInfo(buf, h)
+	if err != nil {
+		log.Printf("%s failed to parse header: %v", addr, err)
+		return
+	}
+	log.Printf("%s packet=%x scid=%x dcid=%x", addr,
+		h.Type, h.SCID, h.DCID)
+	c, ok := s.conns[string(h.DCID)]
+	if !ok {
+		if h.Version != quiche.ProtocolVersion {
+			err = s.negotiate(addr, h, buf)
+			if err != nil {
+				log.Printf("%s failed to write version negotiation: %v", addr, err)
+			} else {
+				log.Printf("%s negotiate version: %x", addr, h.Version)
+			}
+			return
+		}
+		var scid, odcid []byte
+		if s.noRetry {
+			scid = newConnID()
+		} else {
+			if len(h.Token) == 0 {
+				scid = newConnID()
+				err = s.retry(addr, h, scid, buf)
+				if err != nil {
+					log.Printf("%s failed to write stateless retry: %v", addr, err)
+				} else {
+					log.Printf("%s stateless retry: %x", addr, scid)
+				}
+				return
+			}
+			odcid = s.validateToken(addr, h.Token)
+			if len(odcid) == 0 {
+				log.Printf("%s invalid address validation token", addr)
+				return
+			}
+			scid = h.DCID
+		}
+		c = serverConn{
+			addr: addr,
+			conn: quiche.Accept(scid, odcid, s.config),
+		}
+		s.conns[string(scid)] = c
+		log.Printf("%s new connection: %x", addr, scid)
+	}
+	_, err = c.conn.Recv(buf)
+	if err == quiche.ErrDone {
+		return
+	}
+	if err != nil {
+		log.Printf("%s failed to process packet: %v", addr, err)
+		c.conn.Close(false, 0x1, []byte("fail"))
+		return
+	}
+	if c.conn.IsEstablished() {
+		s.recvStream(c.conn, buf)
+	}
+}
+
+func (s *server) headerInfo(buf []byte, h *quiche.Header) error {
+	h.SCID = h.SCID[:cap(h.SCID)]
+	h.DCID = h.DCID[:cap(h.DCID)]
+	h.Token = h.Token[:cap(h.Token)]
+	return quiche.HeaderInfo(buf, quiche.MaxConnIDLen, h)
+}
+
+func (s *server) negotiate(addr net.Addr, h *quiche.Header, buf []byte) error {
+	n, err := quiche.NegotiateVersion(h.SCID, h.DCID, buf[:maxDatagramSize])
+	if err != nil {
+		return err
+	}
+	_, err = s.socket.WriteTo(buf[:n], addr)
+	return err
+}
+
+func (s *server) retry(addr net.Addr, h *quiche.Header, scid, buf []byte) error {
+	token := s.mintToken(addr, h)
+	n, err := quiche.Retry(h.SCID, h.DCID, scid, token, buf[:maxDatagramSize])
+	if err != nil {
+		return err
+	}
+	_, err = s.socket.WriteTo(buf[:n], addr)
+	return err
+}
+
+// TODO: crypto
+func (s *server) mintToken(addr net.Addr, h *quiche.Header) []byte {
+	token := make([]byte, 0, maxTokenLen)
+	token = append(token, []byte("quiche")...)
+	token = append(token, []byte(addr.String())...)
+	token = append(token, h.DCID...)
+	return token
+}
+
+// TODO: crypto
+func (s *server) validateToken(addr net.Addr, token []byte) []byte {
+	if len(token) < 6 || string(token[:6]) != "quiche" {
+		return nil
+	}
+	token = token[6:]
+	addrStr := addr.String()
+	if len(token) < len(addrStr) || string(token[:len(addrStr)]) != addrStr {
+		return nil
+	}
+	return token[len(addrStr):]
+}
+
+func (s *server) recvStream(conn *quiche.Connection, buf []byte) {
+	buf = buf[:cap(buf)]
+	for {
+		id, ok := conn.ReadableNext()
+		if !ok {
+			return
+		}
+		n, fin, err := conn.StreamRecv(id, buf)
+		if err != nil {
+			log.Printf("stream %d recv failed: %v", id, err)
+			continue
+		}
+		log.Printf("stream %d has %d bytes (fin=%v)\n%s", id, n, fin, buf[:n])
+		_, err = conn.StreamSend(httpRequestStreamID, []byte("Not Found"), true)
+		if err != nil {
+			log.Printf("stream send failed: %v", err)
+			conn.Close(false, 0x1, []byte("fail"))
+		}
+	}
+}
+
+func (s *server) send(buf []byte) error {
+	buf = buf[:maxDatagramSize]
+	for _, c := range s.conns {
+		n, err := c.conn.Send(buf)
+		if err == quiche.ErrDone {
+			log.Printf("%s done writing", c.addr)
+			continue
+		}
+		if err != nil {
+			log.Printf("%s send failed: %v", c.addr, err)
+			c.conn.Close(false, 0x1, []byte("fail"))
+			continue
+		}
+		n, err = s.socket.WriteTo(buf[:n], c.addr)
+		if err != nil {
+			return err
+		}
+		log.Printf("%s written %d bytes", c.addr, n)
+	}
+	return nil
+}
+
+func (s *server) close() {
+	var stats quiche.Stats
+	for k, c := range s.conns {
+		if c.conn.IsClosed() {
+			c.conn.Stats(&stats)
+			log.Println("connection closed:", &stats)
+			delete(s.conns, k)
+			c.conn.Free()
+		}
+	}
+}
+
+func serverCommand(args []string) error {
+	cmd := flag.NewFlagSet("server", flag.ExitOnError)
+	verbose := cmd.Bool("v", false, "enable debug logging")
+	listenAddr := cmd.String("listen", "localhost:4433", "listen on the given IP:port")
+	certFile := cmd.String("cert", "cert.crt", "TLS certificate path")
+	keyFile := cmd.String("key", "cert.key", "TLS certificate key path")
+	rootPath := cmd.String("root", ".", "root directory")
+	cmd.Parse(args)
+
+	if *verbose {
+		quiche.EnableDebugLogging()
+	}
+	config, err := newConfig(quiche.ProtocolVersion)
+	if err != nil {
+		return err
+	}
+	defer config.Free()
+	if *certFile != "" {
+		err = config.LoadCertChainFromPEMFile(*certFile)
+		if err != nil {
+			return err
+		}
+	}
+	if *keyFile != "" {
+		err = config.LoadPrivKeyFromPEMFile(*keyFile)
+		if err != nil {
+			return err
+		}
+	}
+	return listen(config, *listenAddr, *rootPath)
+}
